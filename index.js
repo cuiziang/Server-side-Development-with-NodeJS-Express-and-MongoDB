5d5ea699b1f1f8a67347897635109954c31338e4
Ziang Cui <mrchoisss@gmail.com>
2018-09-16 01:44:29 -0400

    Express Router

--- index.js
+++ index.js
@@ -1,24 +1,30 @@
 const express = require('express');
 const http = require('http');
 const morgan = require('morgan');
+const bodyParser = require('body-Parser');
+
+const dishRouter = require('./routes/dishRouter');
 
 const hostname = 'localhost';
 const port = '3000';
 
-const app =express();
+const app = express();
 app.use(morgan('dev'));
+app.use(bodyParser.json());
+
+app.use('/dishes', dishRouter);
 
 app.use(express.static(__dirname + '/public'));
 
-app.use((req,res,next)=>{
-   res.statusCode =200;
-   res.setHeader('Content-Type','text/html');
+app.use((req, res, next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
     res.end('<html><body><h1>This is an Express Server</h1></body></html>')
 });
 
 
-const server =http.createServer(app);
+const server = http.createServer(app);
 
-server.listen(port,hostname,()=>{
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}`)
 });
