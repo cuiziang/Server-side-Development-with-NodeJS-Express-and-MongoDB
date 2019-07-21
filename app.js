22f59ceb54b3a7725d387b51adc07de0d25a64ea
Ziang Cui <mrchoisss@gmail.com>
2019-07-20 22:50:17 -0400

    HTTPS

--- app.js
+++ app.js
@@ -31,6 +31,17 @@ var leaderRouter = require('./routes/leaderRouter');
 
 var app = express();
 
+// Secure traffic only
+app.all('*', (req, res, next) => {
+    if (req.secure) {
+        return next();
+    }
+    else {
+        res.redirect(307, 'https://' + req.hostname + ':' + app.get('secPort') + req.url);
+    }
+});
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
