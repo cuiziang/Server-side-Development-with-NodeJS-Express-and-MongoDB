1304a7c5a43bd46448fa348e6b00b9f6a35d49c7
Ziang Cui <mrchoisss@gmail.com>
2019-07-21 02:55:37 -0400

    Passport Facebook

--- authenticate.js
+++ authenticate.js
@@ -4,6 +4,7 @@ const User = require('./models/user')
 const JwtStrategy = require('passport-jwt').Strategy
 const ExtractJwt = require('passport-jwt').ExtractJwt
 const jwt = require('jsonwebtoken') // used to create, sign, and verify tokens
+const FacebookTokenStrategy = require('passport-facebook-token')
 
 const config = require('./config.js')
 
@@ -46,3 +47,30 @@ exports.verifyAdmin = (req, res, next) => {
         return next(err);
     }
 };
+
+exports.facebookPassport = passport.use(new FacebookTokenStrategy({
+        clientID: config.facebook.clientId,
+        clientSecret: config.facebook.clientSecret
+    }, (accessToken, refreshToken, profile, done) => {
+        User.findOne({facebookId: profile.id}, (err, user) => {
+            if (err) {
+                return done(err, false);
+            }
+            if (!err && user !== null) {
+                return done(null, user);
+            }
+            else {
+                user = new User({ username: profile.displayName });
+                user.facebookId = profile.id;
+                user.firstname = profile.name.givenName;
+                user.lastname = profile.name.familyName;
+                user.save((err, user) => {
+                    if (err)
+                        return done(err, false);
+                    else
+                        return done(null, user);
+                })
+            }
+        });
+    }
+));
